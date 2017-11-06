a5db1f21aeebf77737cdbd7ccd25e794464fff56
Blerina Gkotse <blerina.gkotse@cern.ch>
2017-11-06 12:04:42 +0100

    reconfiguring wsgi.py

--- test_samples_manager/test_samples_manager/wsgi.py
+++ test_samples_manager/test_samples_manager/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-"""import os, sys
+import os, sys
 
 sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR']))
 
@@ -18,31 +18,6 @@ from django.core.wsgi import get_wsgi_application
 
 
 
-application = get_wsgi_application()"""
-
-#!/usr/bin/python
-import os, sys
-
-sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR']))
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'test_samples_manager.settings'
-
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-
-os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
-
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-try:
-    execfile(virtualenv, dict(__file__=virtualenv))
-except IOError:
-    pass
-
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
-
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 
