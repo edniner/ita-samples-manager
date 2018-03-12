36aaeb8495556fdc5d5b279f65a7ce9c67074a20
bgkotse <blerina.gkotse@cern.ch>
2018-03-12 18:08:07 +0100

    adding ModelBackend

--- test-irrad-management-page/settings.py
+++ test-irrad-management-page/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE_CLASSES = (
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.RemoteUserBackend',
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 ROOT_URLCONF = 'test-irrad-management-page.urls'
