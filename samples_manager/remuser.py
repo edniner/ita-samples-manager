87d23b701978f3e6b6406e3d24cea527d9a6f9bd
bgkotse <blerina.gkotse@cern.ch>
2018-03-13 16:50:45 +0100

    adding header HTTP_X_REMOTE_USER

--- samples_manager/remuser.py
+++ samples_manager/remuser.py
@@ -2,7 +2,7 @@ from django.contrib.auth.middleware import RemoteUserMiddleware
 
 # customising the RemoteUserMiddleware to get the HTTP header that openshift adds
 class ProxyRemoteUserMiddleware(RemoteUserMiddleware):
-    '''header = 'HTTP_X_REMOTE_USER' '''
+    header = 'HTTP_X_REMOTE_USER'
     def process_request(self,request):
         if hasattr(request, 'user') and request.user.is_authenticated() \
          and request.user.get_username() == request.META[self.header]:
