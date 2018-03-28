41337bdf0b1ec34e98ed02525d0e53be586aa434
bgkotse <blerina.gkotse@cern.ch>
2018-03-28 12:52:16 +0200

    settings for deployment

--- samples_manager/views.py
+++ samples_manager/views.py
@@ -28,15 +28,15 @@ def send_mail_notification(title,message,from_mail,to_mail):
     msg.send()
 
 def get_logged_user(request):
-    '''username =  request.META["HTTP_X_REMOTE_USER"]
+    username =  request.META["HTTP_X_REMOTE_USER"]
     firstname = request.META["HTTP_X_REMOTE_USER_FIRSTNAME"]
     lastname = request.META["HTTP_X_REMOTE_USER_LASTNAME"]
     email =  request.META["HTTP_X_REMOTE_USER_EMAIL"]
-    '''
-    username =  "bgkotse"
+
+    '''username =  "bgkotse"
     firstname =  "Blerina"
     lastname = "Gkotse"
-    email =  "blerina.gkotse@cern.ch"
+    email =  "blerina.gkotse@cern.ch"'''
 
     users = Users.objects.all()
     emails =[]
